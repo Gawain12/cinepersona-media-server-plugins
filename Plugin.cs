dc5225ac56f299c2b0d123a07b8e6d12a5961fbf
gawain <tankaiyuan33@gmail.com>
2026-09-10 12:16:29 +0800

    fix: add Emby catalog image and config save feedback

--- Plugin.cs
+++ Plugin.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using Emby.Plugin.CinePersona.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Drawing;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
 namespace Emby.Plugin.CinePersona
 {
-    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages, IHasThumbImage
     {
+        private const string ThumbImageResource = "Emby.Plugin.CinePersona.assets.emby-catalog-thumb.png";
+
         public static Plugin Instance { get; private set; }
 
         public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
@@ -23,6 +27,13 @@ namespace Emby.Plugin.CinePersona
         public override string Description => "自动同步 Emby 观影记录到 CinePersona";
 
         public override Guid Id => Guid.Parse("f62e8471-469b-43d8-b57f-f4a4d7d10001");
+
+        public Stream GetThumbImage()
+        {
+            return GetType().Assembly.GetManifestResourceStream(ThumbImageResource);
+        }
+
+        public ImageFormat ThumbImageFormat => ImageFormat.Png;
 
         public IEnumerable<PluginPageInfo> GetPages()
         {
